a677c2d74b60e6ddd2fa7030998070380b1c2545
kebyu007 <kebyu001@gmail.com>
2026-02-16 12:12:43 +0500

    first commit

---
+++ mn.js
@@ -0,0 +1,13 @@
+const inputL = document.querySelector("#inputl");
+const btn = document.querySelector("#btn");
+const img = document.querySelector("#image");
+
+btn.addEventListener("click", () => {
+  let link = inputL.value.trim();
+
+  img.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    encodeURIComponent(link);
+    link.value=""
+    img.style.display="block"
+});
